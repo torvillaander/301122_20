45701a073bc45d709b17b065cb403b31f2993adf
Darin Torres <darintorres2015@gmail.com>
2017-03-30 15:55:09 -0500

    Plantilla
    Estructura de la plantilla del Ovi

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Ovi Conexion php y mysql</title>
+</head>
+
+<body>
+	<div id="ovi">
+    	<header>Aqui va el menu</header>
+        <div id="main">Aqui va el contenido detodo el material del ovi</div>
+        <footer>Aqui va el pie de pagina</footer>
+    </div>
+    
+</body>
+</html>
